c152c7de459b380f4af8e1133f02f4c47b9f957e
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-13 00:18:54 +0300

    UI

--- lib/main.dart
+++ lib/main.dart
@@ -15,10 +15,15 @@ void main() {
         )
       ),
       child: const Center(
-        child: Text('Hello, World!')
+        child: Text('Hello, World!',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.white,
+            ),
       ),
     ),
   ),
-)
+),
+  ),
   );
 }
