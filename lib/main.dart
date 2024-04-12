34b665de842f7d585157aba5e80c2ef55908346c
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-12 23:08:54 +0300

    ap

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: Text('Hello, World!'))));
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center (
+        child: Text('Hello, World!')
+      )
+    )
+  )
+);
 }
