4519cbff20f97ae13ebf4882883a883ce5ad3ac9
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-14 23:46:41 +0300

    build widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-
 void main() {
   runApp(
-  MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
             colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 52, 4, 160),
+              Color.fromARGB(255, 26, 2, 80),
+              Color.fromARGB(255, 52, 4, 160),
             ],
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-        )
-      ),
-      child: const Center(
-        child: Text('Hello, World!',
-          style: TextStyle(
-            fontSize: 28,
-            color: Colors.white,
+          )),
+          child: const Center(
+            child: Text(
+              'Hello, World!',
+              style: TextStyle(
+                fontSize: 28,
+                color: Colors.white,
+              ),
             ),
           ),
         ),
       ),
     ),
-  ),
- );
+  );
 }
 
-
+class GradientContaniner extends StatelessWidget {
+  @override
+  Widget build(){}
+}
