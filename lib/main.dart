7345143f30d0ac3030aa8838652aa3df18d60a44
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-04-14 23:35:29 +0300

    back

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
+
 void main() {
   runApp(
   MaterialApp(
@@ -20,10 +21,12 @@ void main() {
             fontSize: 28,
             color: Colors.white,
             ),
+          ),
+        ),
       ),
     ),
   ),
-  ),
-  ),
-  );
+ );
 }
+
+
