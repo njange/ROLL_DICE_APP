bee07fbd384cc87864a634813d4219f422e547f0
Githuo Njange <128843179+njange@users.noreply.github.com>
2024-05-03 13:34:58 +0300

    widgets

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -8,55 +8,33 @@ const endAlignment = Alignment.bottomRight;
 
 // ignore: must_be_immutable
 class GradientContaniner extends StatelessWidget {
-   GradientContaniner({super.key, required this.colors});
+  GradientContaniner({super.key, required this.colors});
 
-   GradientContaniner.colors()
-   : colors = const [
-    Colors.deepPurple, 
-    Colors.indigo
-    ];
+  GradientContaniner.colors()
+      : colors = const [Colors.deepPurple, Colors.indigo];
 
-var activeDiceImage = 'assets/images/dice-1.png';
+  var activeDiceImage = 'assets/images/dice-1.png';
 
-void rollDice() {
-  activeDiceImage = 'assets/images/dice-2.png';
-}
+  void rollDice() {
+    activeDiceImage = 'assets/images/dice-2.png';
+    print('Changing image...');
+  }
 
   final List<Color> colors;
 
   @override
   Widget build(context) {
     return Container(
-      decoration: BoxDecoration(
-          gradient: LinearGradient(
-        colors: colors,
-        begin: Alignment.topLeft,
-        end: Alignment.bottomRight,
-      )
-      ),
-      child:  Center(
-        child: Column(
-          children: [
-            Image.asset(
-              "assets/images/dice-1.png",
-              width: 200,
-        ),
-        const SizedBox(height:20),
-        TextButton(
-          onPressed: rollDice,
-          style: TextButton.styleFrom(
-            padding: EdgeInsets.only(top: 20),
-            foregroundColor: Colors.white,
-            textStyle: const TextStyle(
-              fontSize: 30,
-            )
-          ),
-        child: const Text('Roll Dice'),
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+          colors: colors,
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        )),
+        child: Center(
+          child: 
         )
-          ],
-        ),
-    )
-    );
+      );
   }
 }
 
--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,9 @@ void main() {
   runApp(
      MaterialApp(
       home: Scaffold(
-        body: GradientContainer(
-          const Color.fromARGB(250, 33, 5, 109),
-          const Color.fromARGB(250,68, 21, 149),
+        body: const GradientContainer(
+          Color.fromARGB(250, 33, 5, 109),
+          Color.fromARGB(250,68, 21, 149),
         ),
       ),
     ),
